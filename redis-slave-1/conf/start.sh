00d59b2ef6d337aab69fd6f1b1ac0cb42ab1fcc6
luoding <luoding@banmatrip.com>
2020-04-04 12:03:32 +0800

    init

---
+++ redis-slave-1/conf/start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# 解决报错：WARNING: The TCP backlog setting of 511 cannot be enforced because /proc/sys/net/core/somaxconn is set to the lower value of 128.
+echo 511 > /proc/sys/net/core/somaxconn
+
+# 解决报错：WARNING: you have Transparent Huge Pages (THP) support enabled in your kernel. This will create latency and memory usage issues with Redis. To fix thisissue run the command 'echo never > /sys/kernel/mm/transparent_hugepage/enabled' as root, and add it to your /etc/rc.local in order to retain thesetting after a reboot. Redis must be restarted after THP is disabled.
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+
+# 指定配置文件
+redis-server /etc/redis/redis.conf
